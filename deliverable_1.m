9842b108afa8f61b4f476644a2bec245d0837fdc
akkoumis <akkoumis@ece.auth.gr>
2020-05-25 19:09:17 +0300

    First Implementation
    Not so good because of edge detectors, probably.

---
+++ deliverable_1.m
@@ -0,0 +1,95 @@
+clear all;
+%% Load image 2 and convert it to gray-scale %%
+im2_rgb = imread('images\im2.jpg');
+im2 = rgb2gray(im2_rgb);
+im2 = double(im2) / 255;
+figure('Name','Initial Image - Grayscale');
+imshow(im2);
+%imwrite(x,'lena_grey.bmp');
+
+%% Edge Detector %%
+im2_canny = edge(im2,'Canny');
+figure('Name','Binary Image - Canny Edge Detection');
+imshow(im2_canny);
+
+im2_sobel = edge(im2);
+figure('Name','Binary Image - Sobel Edge Detection');
+imshow(im2_sobel);
+
+%% Hough Transform - Canny %%
+[H_canny,T_canny,R_canny] = hough(im2_canny,'RhoResolution',1,'Theta',-90:1:89);
+figure('Name','Canny');
+subplot(2,1,1);
+
+imshow(imadjust(rescale(H_canny)),'XData',T_canny,'YData',R_canny,...
+      'InitialMagnification','fit');
+title('Hough transform of Canny');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+colormap(gca,hot);
+P_canny  = houghpeaks(H_canny,10,'threshold',ceil(0.3*max(H_canny(:))));
+x = T_canny(P_canny(:,2)); y = R_canny(P_canny(:,1));
+plot(x,y,'s','color','blue');
+hold off;
+
+subplot(2,1,2);
+lines = houghlines(im2_canny,T_canny,R_canny,P_canny,'FillGap',10,'MinLength',7);
+imshow(im2_rgb), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
+title('Canny');
+hold off;
+
+%% Hough Transform - Sobel %%
+[H_sobel,T_sobel,R_sobel] = hough(im2_sobel,'RhoResolution',1,'Theta',-90:1:89);
+figure('Name','Sobel');
+subplot(2,1,1);
+imshow(imadjust(rescale(H_sobel)),'XData',T_sobel,'YData',R_sobel,...
+      'InitialMagnification','fit');
+title('Hough transform of Sobel');
+xlabel('\theta'), ylabel('\rho');
+axis on, axis normal, hold on;
+colormap(gca,hot);
+P_sobel  = houghpeaks(H_sobel,5,'threshold',ceil(0.3*max(H_sobel(:))));
+x = T_sobel(P_sobel(:,2)); y = R_sobel(P_sobel(:,1));
+plot(x,y,'s','color','blue');
+hold off;
+
+subplot(2,1,2);
+lines = houghlines(im2_sobel,T_sobel,R_sobel,P_sobel,'FillGap',5,'MinLength',7);
+imshow(im2_rgb), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
+title('Sobel');
+hold off;
+
+%% My Hough Transform %%
+[my_H_canny,my_T_canny,my_R_canny] = myHoughTransform(im2_canny,1,pi/180,1);
--- myHoughTransform.m
+++ myHoughTransform.m
@@ -1,7 +1,79 @@
 function [H, L, res] = myHoughTransform(img_binary , Drho , Dtheta , n)
-%MYHOUGHTRANSFORM Summary of this function goes here
+%MYHOUGHTRANSFORM - The Hough Transform of binary image
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%   --- Input ---
+%   img_binary is a binary image (black-white), a product of Thresholding
+%   on a greyscale image.
+%   Drho is the resolution (in pixels) of the rho axis in the parameter space.
+%   Dtheta is the resolution (in rads) of the theta axis in the parameter space.
+%   n is the number of the strongest lines to be returned, based on the
+%   votes in the cells of the Hough matrix.
+%   --- Output ---
+%   H is the voting matrix of the Hough Tranform.
+%   L
+%   res
+
+N1 = size(img_binary, 1);
+N2 = size(img_binary, 2);
+%rho_max = sqrt(N1.^2 + N2.^2);
+rho_max = round(sqrt(N1.^2 + N2.^2));
+%theta_max = 2 * pi;
+theta_max = pi/2;
+T = ceil(2*theta_max/Dtheta);
+R = ceil(2*rho_max/Drho);
+R=R-1;
+
+%thetas = linspace(0, theta_max, T+1);
+thetas = linspace(-theta_max, theta_max, T+1); % Slice the theta axis.
+%rhos = linspace(0, rho_max, R+1);
+rhos = linspace(-rho_max, rho_max, R+1); % Slice the rho axis.
+
+H = zeros(R, T);
+
+for n1=1:N1
+    for n2=1:N2
+        if (img_binary(n1,n2)==1)
+            
+            for j=1:T
+                %t = (thetas(j) + thetas(j+1))/2;
+                t = thetas(j);
+                r = n1*cos(t) + n2*sin(t);
+                dists = rhos-r;
+                [d,i]=min(abs(dists));
+                if (dists(i)<0)
+                   %i=i-1; 
+                end
+                
+                if (d<1)
+                    H(i,j)=H(i,j)+1;
+                    break;
+                end
+            end
+        end
+    end
+end
+
+%{
+for j=1:T
+    t = (thetas(j) + thetas(j+1))/2;
+    c=cos(t);
+    s=sin(t);
+    [row,col] = find(img_binary);
+    n_max=length(row);
+    
+    for n=1:n_max
+        r = row(n)*c + col(n)*s;
+        for i=2:R+1
+            if (rhos(i)>=r)
+                H(i-1,j)=H(i-1,j)+1;
+                break;
+            end
+        end
+    end
+end
+%}
+
+L=1;
+res=1;
 end
 
