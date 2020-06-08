3bf2fc19e77b9a98770f5fc97aed2b9856aeda4d
akkoumis <akkoumis@ece.auth.gr>
2020-06-08 19:37:36 +0300

    Finished Harris with comments and results

--- deliverable_2.m
+++ deliverable_2.m
@@ -2,24 +2,29 @@ clear all;
 %% Load image 2 and convert it to gray-scale %%
 im2_rgb = imread('images\im2.jpg');
 im2 = rgb2gray(im2_rgb);
-im2 = imresize(im2,1/10);
+im2 = imresize(im2,1/5);
 im2 = double(im2) / 255;
 figure('Name','Initial Image - Grayscale');
 imshow(im2);
 
 %% Matlab %%
-corners=detectHarrisFeatures(im2,'MinQuality',0.1);
-figure
+tic;
+% corners=detectHarrisFeatures(im2,'MinQuality',0.1);
+corners=detectHarrisFeatures(im2,'FilterSize', 5);
+matlab_time = toc;
+figure('Name','Matlab Harris');
 imshow(im2);
 hold on
 plot(corners);
 hold off
 
 %% My Harris Corner %%
+tic;
 my_corners = myDetectHarrisFeatures(im2);
-figure
+my_time = toc;
+figure('Name','My Harris');
 imshow(im2);
 hold on
 % plot(my_corners(:,2),my_corners(:,1),'rs','MarkerSize',10);
-plot(my_corners(:,2),my_corners(:,1),'r+');
+plot(my_corners(:,2),my_corners(:,1),'rs');
 hold off
--- myDetectHarrisFeatures.m
+++ myDetectHarrisFeatures.m
@@ -1,52 +1,68 @@
 function corners = myDetectHarrisFeatures(I)
-%MYDETECTHARRISFEATURES Summary of this function goes here
-%   Detailed explanation goes here
+%MYDETECTHARRISFEATURES - Detect corners on given image (I), using
+%Harris-Stephens algorithm.
+%   At first, we assign values to the parameters kappa and threshold. Then,
+%   we initialize the 2D Gaussian filter, with its parameters (w_size and sigma).
+%   Furthermore, we initialize the derivative masks for dx and dy and
+%   filter the image I with those two masks. Using the results, we can
+%   create helper matrices for calculating the A matrix of the algorithm.
+%   Finally, for each pixel we apply the algorithm, by convoluting it with the
+%   filter w and, thus, calculate the matrix M. Using the M, we can decide
+%   whether it's a corner or not.
+%
+%   I is the image whom corners we must detect.
 
-kappa = 0.14;
-threshold = 0.05;
+kappa = 0.04; % Kappa coefficient
+threshold = 0.1; % The T coefficient.
 
 max_Y = size(I,1);
 max_X = size(I,2);
 
-%M = zeros(max_Y, max_X);
 R = zeros(max_Y, max_X);
 
-sigma = 1;
-w_radius = 4;
-w_size = 2*w_radius+1;
-w = fspecial('gaussian',w_size,sigma);
+w_radius = 2; % The radius of the 2D Gaussian filter.
+w_size = 2*w_radius+1; % The size of the 2D Gaussian filter, for each dimension.
+sigma = w_size/3; % The standard deviation of the 2D Gaussian filter.
+w = fspecial('gaussian',w_size,sigma); % Create the 2D Gaussian Filter.
 
-[dx, dy] = meshgrid(-1:1, -1:1);
+[dx, dy] = meshgrid(-1:1, -1:1); % Create the derivative masks for both dimensions.
 
-I1 = conv2(I,dx,'same');
-I2 = conv2(I,dy,'same');
+I1 = conv2(I,dx,'same'); % Calculate the dx derivative by convoluting the mask.
+I2 = conv2(I,dy,'same'); % Calculate the dy derivative by convoluting the mask.
 
-I1sq = I1.^2;
-I2sq = I2.^2;
-I12 = I1.*I2;
+I1sq = I1.^2; % Helper matrix for M calculations.
+I2sq = I2.^2; % Helper matrix for M calculations.
+I12 = I1.*I2; % Helper matrix for M calculations.
 
-corners=zeros(1,2);
+corners=zeros(1,2); % Initialize the cornerns matrix to be returned. The first pair will be discarded at the end.
 
-for p2=1+w_radius:max_Y-w_radius
-   for p1=1+w_radius:max_X-w_radius
-       M = zeros (2,2);
-       for u2=-w_radius:w_radius
-           for u1=-w_radius:w_radius
-               M = M + w(u2+w_radius+1,u1+w_radius+1)*[I1sq(p2+u2,p1+u1) I12(p2+u2,p1+u1); I12(p2+u2,p1+u1) I2sq(p2+u2,p1+u1)];       
-           end
-       end
-%        A = [I1sq(p2,p1) I12(p2,p1); I12(p2,p1) I2sq(p2,p1)]; 
-       
-       
-       R(p2,p1) =  det(M)-kappa*(trace(M).^2);
-       if (R(p2,p1)>threshold)
-           temp =  [p2 p1];
-          corners(size(corners,1)+1,:) = temp; 
-       end
-   end    
+% Iterate through all the pixels (p2,p1) of the image.
+% for p2=1+w_radius:max_Y-w_radius
+%     for p1=1+w_radius:max_X-w_radius
+for p2=1:max_Y
+    for p1=1:max_X
+        M = zeros (2,2);
+        % Convolute with the 2D Gaussian filter with the A matrix.
+        for u2=max(-w_radius,-p2+1):min(w_radius,-p2+max_Y)
+            for u1=max(-w_radius,-p1+1):min(w_radius,-p1+max_X)
+                A = [I1sq(p2+u2,p1+u1) I12(p2+u2,p1+u1); I12(p2+u2,p1+u1) I2sq(p2+u2,p1+u1)];
+                M = M + w(u2+w_radius+1,u1+w_radius+1)*A;
+            end
+        end
+        
+        % Calculate the result, using the M matrix, as stated in the algorithm.
+        R(p2,p1) =  det(M)-kappa*(trace(M).^2);
+        if (R(p2,p1)>threshold) % Check if result passes threshold.
+            temp =  [p2 p1];
+            corners(size(corners,1)+1,:) = temp; % If yes, add the pair to the corners.
+        end
+    end
 end
 
-corners = corners(2:end,:);
-
+if(size(corners,1)>1)
+    corners = corners(2:end,:); % Discard the first zero pair.
+else
+    clear corners;
+end
 end
 
